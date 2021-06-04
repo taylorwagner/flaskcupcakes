872f25bced730aa73a027cfb6911227f232d11f0
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-06-04 14:00:59 -0500

    added and commented out the form for creating a new cupcake

--- forms.py
+++ forms.py
@@ -1,3 +1,10 @@
 # from flask_wtf import FlaskForm
-# from wtforms import StringField, IntegerField, BooleanField, SelectField
-# from wtforms.validators import InputRequired, NumberRange, URL, Optional
+# from wtforms import StringField, FloatField
+# from wtforms.validators import InputRequired, URL
+
+# class AddNewCupcakeForm(FlaskForm):
+#     """Create form for adding a new cupcake"""
+#     flavor = StringField("Cupcake Flavor", validators=[InputRequired("Must include a flavor")])
+#     size = StringField("Cupcake Size", validators=[InputRequired("Must include a size")])
+#     rating = FloatField("Cupcake Rating", validators=[InputRequired("Must include a rating")])
+#     image = StringField("Image URL of Cupcake", validators=[InputRequired("Must include an image URL"), URL()])
